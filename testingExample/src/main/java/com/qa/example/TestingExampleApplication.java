ac2757f462b02b277582911c6d7cd10af3548937
Dale <dale.carr@qa.com>
2019-11-26 14:31:45 +0000

    Initial Commit

---
+++ testingExample/src/main/java/com/qa/example/TestingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.qa.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingExampleApplication.class, args);
+	}
+
+}
---
+++ testingExample/src/test/java/com/qa/example/TestingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
